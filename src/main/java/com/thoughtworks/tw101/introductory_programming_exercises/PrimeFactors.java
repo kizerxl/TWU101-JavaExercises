915f8c812739186e8faf3eebc74467df73623912
kizerxl <changoof@gmail.com>
2018-05-07 23:31:54 -0400

    Add solution to PrimeFactor exercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,15 +6,30 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        System.out.println(primeFactors.toString());
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        int number = n;
+
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2; i <= number / i; i++) {
+            while (number % i == 0) {
+                factors.add(i);
+                number /= i;
+            }
+        }
+        if (number > 1) {
+            factors.add(number);
+        }
+        return factors;
     }
 
 }
